400bbe268b96e28d3b7ef3f0be26a085a5bab818
vedvy <vedant.vyas@stonybrook.edu>
2025-06-10 23:03:21 -0400

    checkpoint, look into how to shrink the div properly for the text

--- my-app/src/components/home_view.jsx
+++ my-app/src/components/home_view.jsx
@@ -1,9 +1,27 @@
+import InstagramLogo from "../images/instagram-logo-instagram-icon-transparent-free-png.webp"
+import TikTokLogo from "../images/tiktoklogo.png"
 
 export default function HomePage()
 {
     return(
-        <div>
-            <h1>Home Page</h1>
+        <div className="home_page">
+            <div className="home_page_details">
+                <h1>Welcome to Roshi Designs</h1>
+                <br/>
+                <br/>
+                <p>Here we sell quality and homemade garlands, baskets, and more!</p>
+                <br/>
+                <br/>
+                <p>Follow us on our social medias to learn about updates to our
+                    store front and new product arrivals! Also click the TikTok link to join our livestreams 
+                    and watch how we create our products by hand!
+                </p>
+                
+            </div>
+            <div className="social_media_logos">
+                    <img src={InstagramLogo} style={{width: "140px"}}/>
+                    <img src={TikTokLogo} style={{width: "100px"}}/>
+            </div>
         </div>
     );
 }
