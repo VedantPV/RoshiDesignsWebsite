19cfb50583aa6ba562c6d0a5cce620442a4e0cc6
vedvy <vedant.vyas@stonybrook.edu>
2025-06-27 20:16:14 -0400

    rentals page in progress, need more images to complete it like the Ganesha statue

--- my-app/src/components/custom_purchases_view.jsx
+++ my-app/src/components/custom_purchases_view.jsx
@@ -1,3 +1,5 @@
+import FlowerWall1 from "../images/roshi_rental_flowerwall_1.jpg";
+import FlowerWall2 from "../images/roshi_rental_flowerwall_2.jpg";
 
 export default function CustomPurchasePage()
 {
@@ -6,13 +8,12 @@ export default function CustomPurchasePage()
             <div className="custom_purchases_details">
                 <h1>Custom Purchase Page</h1>
                 <br/>
-                <p>We do custom products and designs alongside our current inventory</p>
+                <p>We do custom products and designs alongside our current inventory!</p>
+                {/* <h2 style={{textDecoration: "underline", fontStyle: "italic"}}>Flower Walls</h2>
+                <img src={FlowerWall1} style={{height: "250px", width: "300px"}}></img>
+                <img src={FlowerWall2} ></img> */}
                 
-            </div>
-
-            <div className="custom_purchases_products">
-                <h1>The following gallery below should contain past custom products</h1>    
-            </div>            
+            </div>          
         </div>
     )
 }
--- my-app/src/components/rentals.jsx
+++ my-app/src/components/rentals.jsx
@@ -1,9 +1,20 @@
+import FlowerWall1 from "../images/roshi_rental_flowerwall_1.jpg";
+import FlowerWall2 from "../images/roshi_rental_flowerwall_2.jpg";
 
 export default function RentalPage()
 {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="rentals_page">
+            <div className="rentals_details">
+                <h1 style={{textDecoration: "underline"}}>Only For Current Residents in New York</h1>
+                <p>Rent some of our finest decorations and statues for your special celebrations!</p>
+                <h2 style={{textDecoration: "underline", fontStyle: "italic"}}>Flower Walls</h2>
+                <img src={FlowerWall1} className="rental_images"></img>
+                <p>White Flower Wall</p>
+                <br></br>
+                <img src={FlowerWall2} className="rental_images"></img>
+                <p>Multi-Colored Wall with Flower Design</p>
+            </div>
         </div>
     );
 }
